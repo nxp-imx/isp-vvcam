9d68d66b88b93999ec24e8c849ee3314a795aa1f
Oliver F. Brown <oliver.brown@nxp.com>
2020-06-18 20:06:14 -0700

    updating build script for vvcam
    Signed-off-by: Oliver F. Brown <oliver.brown@nxp.com>

--- build-all-vvcam.sh
+++ build-all-vvcam.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export KERNEL_SOURCE_DIR=/build/users/nxa18621/proj/imx8/linux-imx
-export KERNEL_SRC=/build/users/nxa18621/proj/imx8/linux-imx
+#export KERNEL_SRC=/build/users/nxa18621/proj/imx8/linux-imx
 #export KERNEL_SOURCE_DIR=/build/users/nxa18621/proj/imx8/linux-imx-caf
 
 #manually clean vvcam (if needed)
@@ -12,8 +12,18 @@ find -name *.ko | xargs rm -fv
 find -name *.o.cmd | xargs rm -fv 
 cd -
 
-cd vvcam/common
+cd vvcam/v4l2
 #make -f 1802_chip.mk clean
-make
+make KERNEL_SRC=$KERNEL_SOURCE_DIR
 cd -
+
+rm -rf modules
+mkdir -p modules
+
+cp vvcam/v4l2/vvcam-dwe.ko modules
+cp vvcam/v4l2/sensor/ov2775/ov2775.ko modules
+cp vvcam/v4l2/sensor/os08a20/os08a20.ko modules
+cp vvcam/v4l2/csi/samsung/vvcam-csis.ko modules
+cp vvcam/v4l2/vvcam-isp.ko modules
+cp vvcam/v4l2/video/vvcam-video.ko modules
 
